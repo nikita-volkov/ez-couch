0156ae4f35d74bc18debac16a1041a1280ceb949
Nikita Volkov <justnika@mail.ru>
2013-01-16 06:49:32 +0400

    cleanup

--- src/EZCouch.hs
+++ src/EZCouch.hs
@@ -1,21 +1,8 @@
 {-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts, ScopedTypeVariables, DeriveDataTypeable, DeriveFunctor #-}
 module EZCouch (module Types, module ReadAction, module BulkOperationAction, run, runWithManager) where
 
-import Prelude ()
-import BasicPrelude
-
-import Data.Generics
-
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.Resource
-
 import EZCouch.Action
 import EZCouch.Types as Types
 import EZCouch.ReadAction as ReadAction
 import EZCouch.BulkOperationAction as BulkOperationAction
 -- import EZCouch.Design as Design
-
-import qualified Network.HTTP.Conduit as HTTP
-
-
-
