c47c723b1080c1978854d983480d0bc2adb4ecfb
Nikita Volkov <justnika@mail.ru>
2013-01-18 05:43:53 +0400

    exports and docs

--- src/EZCouch.hs
+++ src/EZCouch.hs
@@ -1,5 +1,42 @@
 {-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts, ScopedTypeVariables, DeriveDataTypeable, DeriveFunctor #-}
-module EZCouch (module Types, module ReadAction, module BulkOperationsAction, module Design, run, runWithManager) where
+module EZCouch (
+  -- * Execution Monad
+  MonadAction(..),
+  run,
+  runWithManager,
+  -- * CRUD Monadic Functions
+  -- | All monadic functions are split into `CRUD` categories. The functions with a 'Multiple' suffix should be used for performing multiple operations at once.
+  
+  -- ** Creating Monadic Functions
+  create,
+  createMultiple,
+  createWithId,
+  createMultipleWithIds,
+  -- ** Reading Monadic Functions
+  readMultiple,
+  readExists,
+  readIds,
+  readKeys,
+  readCount,
+  -- ** Updating Monadic Functions
+  update,
+  updateMultiple,
+  -- ** Deleting Monadic Functions
+  delete,
+  deleteMultiple,
+  -- * Working with Design Documents
+  Design(..),
+  createOrUpdateDesign,
+  readDesign,
+  -- * Types
+  Persisted(..),
+  EZCouchException(..),
+  ReadOptions(..),
+  readOptions,
+  ConnectionSettings(..),
+  defaultPort
+
+) where
 
 import EZCouch.Action
 import EZCouch.Types as Types
