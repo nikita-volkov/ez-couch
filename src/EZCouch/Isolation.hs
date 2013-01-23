dd44e3934ed8b9e072c5c7bd651ec179feb71aff
Nikita Volkov <justnika@mail.ru>
2013-01-23 16:43:49 +0400

    simple isolation

---
+++ src/EZCouch/Isolation.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables, DeriveDataTypeable, DeriveGeneric #-}
+module EZCouch.Isolation where
+
+import Prelude ()
+import ClassyPrelude hiding (delete)
+
+import EZCouch.Types
+import EZCouch.Action
+import EZCouch.BulkOperationsAction
+import EZCouch.Model.Isolation
+
+-- | Protect an action from being executed on multiple clients. Can be used to create transactions in a preemptive manner, i.e. instead of performing some actions and rolling back on transaction validation failure, do the transaction validation based on the provided identifier prior to actually executing the transaction. This function however does not protect from unfinished transactions.
+isolate :: MonadAction m 
+  => Text -- ^ A unique isolation identifier. It's a common practice to provide a 'persistedId' of the primary entity involved in the transaction, which is supposed to uniquely identify it.
+  -> m a -- ^ The action to protect. Nothing of it will be executed if an isolation with the same id is already running.
+  -> m (Maybe a) -- ^ Either the action's result or `Nothing` if it didn't perform.
+isolate id action = do
+  result <- (try $ createWithId id' $ Isolation)
+  case result of
+    Left (OperationException _) -> return Nothing
+    Left e -> throwIO e
+    Right isolation -> do
+      result <- action
+      delete isolation
+      return $ Just result
+  where 
+    id' = "EZCouchIsolation-" ++ id
+
---
+++ src/EZCouch/Model/Isolation.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables, DeriveDataTypeable, DeriveGeneric #-}
+module EZCouch.Model.Isolation where
+
+import Prelude ()
+import ClassyPrelude
+import GHC.Generics
+import EZCouch.Doc
+import Data.Aeson
+import Data.Time
+
+data Isolation 
+  = Isolation 
+  deriving (Show, Eq, Generic)
+instance ToJSON (Isolation)
+instance FromJSON (Isolation)
+instance Doc (Isolation)
