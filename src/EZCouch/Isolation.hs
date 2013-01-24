62db8ba0c8bad495e38f2c3f65094b1989817951
Nikita Volkov <justnika@mail.ru>
2013-01-24 05:23:58 +0400

    Isolation logging

--- src/EZCouch/Isolation.hs
+++ src/EZCouch/Isolation.hs
@@ -2,15 +2,19 @@
 module EZCouch.Isolation where
 
 import Prelude ()
-import ClassyPrelude hiding (delete)
+import ClassyPrelude hiding (delete, log)
 import qualified Data.Time as Time
 
 import EZCouch.Time
 import EZCouch.Types
-import EZCouch.Action
+import EZCouch.Action hiding (log)
 import EZCouch.ReadAction
 import EZCouch.BulkOperationsAction
 import EZCouch.Model.Isolation as Isolation
+
+import qualified Util.Logging as Logging
+
+log lvl = Logging.log "EZCouch.Isolation" lvl
 
 -- | Protect an action from being executed on multiple clients. Can be used to create transactions in a preemptive manner, i.e. instead of performing some actions and rolling back on transaction validation failure, do validation based on the provided identifier prior to actually executing the transaction. This function however does not provide you with guarantees that the action will either be executed in whole or not executed at all, as it does not rollback in case of client-interrupt - it's up to your algorithms to handle those cases.
 isolate :: MonadAction m 
@@ -28,12 +32,19 @@ isolate timeout id action = do
         Just isolation -> do
           if (Isolation.since . persistedValue) isolation < Time.addUTCTime (negate $ fromIntegral timeout) time
             then do 
+              log 0 $ "Deleting outdated isolation: " ++ id'
               tryToDelete isolation
               isolate timeout id action
-            else return Nothing
-        Nothing -> return Nothing
+            else do
+              log 0 $ "Skipping a busy isolation: " ++ id'
+              return Nothing
+        Nothing -> do
+          log 0 $ "Skipping a finished isolation: " ++ id'
+          return Nothing
     Left e -> throwIO e
-    Right isolation -> finally (Just <$> action) (delete isolation)
+    Right isolation -> do
+      log 0 $ "Performing an isolation: " ++ id'
+      finally (Just <$> action) (delete isolation)
   where 
     id' = "EZCouchIsolation-" ++ id
 
