5e0513ae15aa0644a20d11ef1ab67e9686785883
Nikita Volkov <justnika@mail.ru>
2013-01-25 09:30:35 +0400

    fix null-reduce

--- src/EZCouch/Model/View.hs
+++ src/EZCouch/Model/View.hs
@@ -9,7 +9,10 @@ import Data.Aeson
 
 data View = View { map :: Text, reduce :: Maybe Text }
   deriving (Show, Eq, Generic)
-instance ToJSON View
+instance ToJSON View where
+  toJSON (View map reduce) = object $ catMaybes 
+    [ Just ("map", toJSON map), 
+      (,) <$> pure "reduce" <*> toJSON <$> reduce ]
 instance FromJSON View where
   parseJSON = withObject "View" $ \o -> 
     View <$> o .: "map" <*> o .:? "reduce"  
