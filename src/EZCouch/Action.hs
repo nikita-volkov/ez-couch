f5f082904eaa98244a9d2e612217951a0c9bb382
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-01-28 16:04:20 +0400

    reformat

--- src/EZCouch/Action.hs
+++ src/EZCouch/Action.hs
@@ -33,29 +33,29 @@ responseAction method dbPath qps body
   = do
       (settings, manager) <- ask
       let request = settingsRequest settings
-      log 0 
-        $ "Performing a " 
+      log 0 $ 
+        "Performing a " 
           ++ show method ++ " at " 
           ++ show (HTTP.url request)
-      retrying exceptionIntervals 
-        $ (flip catch) handleIOException
-        $ (flip catch) handleHttpException 
-        $ http request manager
+      retrying exceptionIntervals $
+        (flip catch) handleIOException $
+          (flip catch) handleHttpException $
+            http request manager
   where
     headers = [("Content-Type", "application/json")]
     query = renderQuery False $ CC.mkQuery qps
-    settingsRequest (ConnectionSettings host port auth database) 
-      = authenticated $ def {
-          method = method,
-          host = encodeUtf8 host,
-          requestHeaders = headers,
-          port = port,
-          path = packPath $ maybe [] (database : ) $ dbPath,
-          queryString = query,
-          requestBody = RequestBodyLBS body,
-          checkStatus = checkStatus,
-          responseTimeout = Just $ 10 ^ 6 * 10
-        }
+    settingsRequest (ConnectionSettings host port auth database) =
+      authenticated $ def {
+        method = method,
+        host = encodeUtf8 host,
+        requestHeaders = headers,
+        port = port,
+        path = packPath $ maybe [] (database : ) $ dbPath,
+        queryString = query,
+        requestBody = RequestBodyLBS body,
+        checkStatus = checkStatus,
+        responseTimeout = Just $ 10 ^ 6 * 10
+      }
       where
         authenticated
           | Just (username, password) <- auth = applyBasicAuth (encodeUtf8 username) (encodeUtf8 password)
@@ -63,24 +63,24 @@ responseAction method dbPath qps body
     checkStatus status@(Status code message) headers
       | elem code [200, 201, 202, 304] = Nothing
       | otherwise = Just $ SomeException $ StatusCodeException status headers
-    exceptionIntervals (ConnectionException _) = [10^3, 10^6, 10^6*10]
+    exceptionIntervals (ConnectionException {}) = [10^3, 10^6, 10^6*10]
     exceptionIntervals _ = []
     handleHttpException e = case e of
-      FailedConnectionException host port -> throwIO $ ConnectionException 
-        $ "FailedConnectionException: " ++ pack host ++ " " ++ show port
+      FailedConnectionException host port -> throwIO $ ConnectionException $ 
+        "FailedConnectionException: " ++ pack host ++ " " ++ show port
       otherwise -> throwIO e
-    handleIOException e = throwIO $ ConnectionException 
-      $ "IOError: " ++ pack (ioeGetErrorString e)
+    handleIOException e = throwIO $ ConnectionException $ 
+      "IOError: " ++ pack (ioeGetErrorString e)
 
-action method path qps body 
-  = responseBody <$> responseAction method (Just path) qps body 
+action method path qps body = 
+  responseBody <$> responseAction method (Just path) qps body 
 putAction = action HTTP.methodPut
 postAction = action HTTP.methodPost
 getAction = action HTTP.methodGet
 
-
-runWithManager manager settings action = runReaderT action (settings, manager)
-run settings action = HTTP.withManager $ 
-  \manager -> runWithManager manager settings action
+runWithManager manager settings action = 
+  runReaderT action (settings, manager)
+run settings action = HTTP.withManager $ \manager -> 
+  runWithManager manager settings action
 
 packPath = Blaze.toByteString . HTTP.encodePathSegments . filter (/="")
