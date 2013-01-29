7739fe03a4eade7e11ad50ed7f39c4d9485d5766
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-01-29 22:11:52 +0400

    reimplement Logging on ClassyPrelude

--- src/Util/Logging.hs
+++ src/Util/Logging.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Util.Logging where
 
-import Prelude ()
-import BasicPrelude 
+import Prelude (String)
+import ClassyPrelude 
 
 import System.Log.Logger as Logger 
 import System.Log 
@@ -14,19 +15,23 @@ import System.Locale (defaultTimeLocale)
 import Data.Time (getZonedTime, getCurrentTime, formatTime)
 import Control.Concurrent (myThreadId)
 
-initialize = do
+log :: (MonadIO m) => Text -> Int -> Text -> m ()
+log logger level message = liftIO $ 
+  logM (unpack logger) (levelPriority level) (unpack message)
+
+initialize = initializeWithFormat "$level $time, $logger: $message"
+
+initializeWithFormat :: Text -> IO ()
+initializeWithFormat format = do
   updateGlobalLogger "" (Logger.setLevel DEBUG)
 
   removeAllHandlers
   h <- streamHandler stderr DEBUG
-  h <- return $ setFormatter h (formatter "$level $time, $logger: $message")
+  h <- return $ setFormatter h (formatter format)
   updateGlobalLogger "" (setHandlers [h])
 
-log logger level message
-  = liftIO $ logM (textToString logger) (levelPriority level) (textToString message)
-
 setLoggerLevel logger level = do
-  updateGlobalLogger (textToString logger) (Logger.setLevel $ levelPriority level)
+  updateGlobalLogger (unpack logger) (Logger.setLevel $ levelPriority level)
 
 levelPriority level = case level of
   0 -> DEBUG
@@ -38,7 +43,7 @@ levelPriority level = case level of
   6 -> ALERT
   x | x >= 7 -> EMERGENCY
 
-priorityLevel p = head [l | l <- [0..7], p == levelPriority l]
+priorityLevel p = fromMaybe undefined $ find ((==) p . levelPriority) [0..7]
 
 formatter format h (prio, msg) loggername 
   = replaceVarM 
@@ -46,12 +51,12 @@ formatter format h (prio, msg) loggername
         ("time", formatTime defaultTimeLocale timeFormat <$> getZonedTime),
         ("utcTime", formatTime defaultTimeLocale timeFormat <$> getCurrentTime),
         ("message", return msg), 
-        ("priority", return $ textToString $ show prio), 
-        ("level", return $ textToString $ show $ priorityLevel prio), 
+        ("priority", return $ show prio), 
+        ("level", return $ show $ priorityLevel prio), 
         ("logger", return loggername), 
-        ("tid", textToString <$> show <$> myThreadId)
+        ("tid", show <$> myThreadId)
       ]
-      format
+      $ unpack format
   where
     timeFormat = "%F %X %Z"
 
