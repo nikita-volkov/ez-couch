6ae7c43b9e57be8b9d475c5d33b76d3fce8f8afc
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-02-01 14:30:36 +0400

    RandomDemo with Docs

--- src/RandomDemo.hs
+++ src/RandomDemo.hs
@@ -1,10 +1,33 @@
-{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables, DeriveDataTypeable, DeriveGeneric, GADTs, StandaloneDeriving #-}
+-- | This module showcases how EZCouch can be used to elegantly and 
+-- transparantly solve some quite untrivial CouchDB tasks. It shows how you can 
+-- fetch, create and delete entities from the database, how to use EZCouch's 
+-- powerful features, such as Random Fetching, and how EZCouch transparantly 
+-- manages the views for you.
 
+-- These are required flags.
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveGeneric #-}
+
+-- Nothing fancy, we just block the standard Prelude here and import a way more 
+-- convenient one. 
+-- More details about it here: 
+-- http://hackage.haskell.org/package/classy-prelude
 import Prelude ()
 import ClassyPrelude
-import GHC.Generics
+
+-- Generics (or reflection). This allows us to easily generate instances of 
+-- supporting classes without any implementation boilerplate.
+-- This is a modern implementation of generics for Haskell. It is much more 
+-- productive than SYB and may even go on par with TemplateHaskell-based 
+-- solutions.
+import GHC.Generics (Generic)
+
+-- Everything that's needed to use EZCouch.
 import EZCouch
 
+
+-- Connection settings for connecting to a locally running CouchDB with an
+-- existing database named "test" configured to provide free permissions to 
+-- unauthenticated users.
 connection = ConnectionSettings {
   connectionSettingsHost = "127.0.0.1",
   connectionSettingsPort = defaultPort,
@@ -12,30 +35,40 @@ connection = ConnectionSettings {
   connectionSettingsDatabase = "test"
 }
 
+-- An entity of our model. This is the only data that we need to communicate
+-- with db. No intermediate objects or quirky types.
+-- Note: the entity must derive the `Generic` type imported from `GHC.Generics`.
 data A = A {a :: Int, b :: Text, c :: Maybe Double}
   deriving (Show, Generic)
+-- Generate instances of Aeson's typeclasses using generics.
 instance ToJSON A
 instance FromJSON A
+-- Generate an instance of EZCouch's typeclass which is required to be able
+-- to use the entity with db.
 instance Doc A
 
-view1 :: View A (Int, Double)
-view1 = ViewKeys2 (ViewKeyField "b") (ViewKeyRandom)
+-- Execute our program by running actions in the EZCouch's `MonadAction`.
+-- First we regenerate all the entities in the db and then subsequently fetch
+-- and print random ones from it.
+main = run connection $ do
+  regenerateAs
+  printRandomAs
+  printRandomAs
+  printRandomAs
 
 
 regenerateAs = do
-  as <- readEntities (ViewAll :: View A Text) KeysSelectionAll 0 Nothing False
+  -- Fetch all existing entities of type `A`.
+  as :: [Persisted A] <- readEntities ViewAll KeysSelectionAll 0 Nothing False
+  -- Delete them all.
   deleteMultiple as
+  -- Create new ones.
   createMultiple $ map (\i -> A i "a" Nothing) [0..7]
 
-
-main = run connection $ do
-  regenerateAs
-  printRandom
-  printRandom
-  printRandom
-
-printRandom = do  
+printRandomAs = do
+  -- Fetch at most 2 random entities.
   as :: [Persisted A] <- readRandomEntities $ Just 2
+  -- Output info about them.
   liftIO $ putStrLn $ 
     "Got " 
       ++ (show $ length as) ++ " random results: " 
